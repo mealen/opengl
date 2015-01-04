b9e0fa0d9a7a363523cc3191589b4b1780de8291
Engin Manap <enginmanap@gmail.com>
2015-01-04 21:51:10 +0200

    fix tutorial 5

--- 5/src/GlHelper.cpp
+++ 5/src/GlHelper.cpp
@@ -5,15 +5,18 @@ GLuint GlHelper::CreateShader(GLenum eShaderType, const std::string &strShaderFi
     GLuint shader = glCreateShader(eShaderType);
     std::string shaderCode;
     std::ifstream shaderStream(strShaderFile.c_str(), std::ios::in);
-    if(shaderStream.is_open()){
-            std::string Line = "";
-            while(getline(shaderStream, Line))
-                    shaderCode += "\n" + Line;
-            shaderStream.close();
-    }else{
-            printf("Impossible to open %s. Are you in the right directory ? Don't forget to read the FAQ !\n", strShaderFile.c_str());
-            getchar();
-            return 0;
+
+    if(shaderStream.is_open()) {
+        std::string Line = "";
+
+        while(getline(shaderStream, Line))
+            shaderCode += "\n" + Line;
+
+        shaderStream.close();
+    } else {
+        std::cerr << strShaderFile.c_str() << " dosyasi acilamadi. Uygulamanizi bu dosyanin oldugu dizinde calistirdiginizdan emin olunuz." << std::endl;
+        getchar();
+        return 0;
     }
 
     const char* shaderCodePtr = shaderCode.c_str();
@@ -23,6 +26,7 @@ GLuint GlHelper::CreateShader(GLenum eShaderType, const std::string &strShaderFi
 
     GLint status;
     glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+
     if (status == GL_FALSE)
     {
         GLint infoLogLength;
@@ -31,23 +35,27 @@ GLuint GlHelper::CreateShader(GLenum eShaderType, const std::string &strShaderFi
         GLchar *strInfoLog = new GLchar[infoLogLength + 1];
         glGetShaderInfoLog(shader, infoLogLength, NULL, strInfoLog);
         const char *strShaderType = NULL;
+
         switch(eShaderType)
         {
         case GL_VERTEX_SHADER:
             strShaderType = "vertex";
             break;
+
         case GL_GEOMETRY_SHADER:
             strShaderType = "geometry";
             break;
+
         case GL_FRAGMENT_SHADER:
             strShaderType = "fragment";
             break;
         }
 
-        fprintf(stderr, "Compile failure in %s shader:\n%s\n", strShaderType, strInfoLog);
+        std::cerr << strShaderType << " tipi shader dosyasi derlenemedi. Detaylar:\n" << strInfoLog << std::endl;
         delete[] strInfoLog;
 
     }
+
     return shader;
 }
 
@@ -60,6 +68,7 @@ GLuint GlHelper::CreateProgram(const std::vector<GLuint> &shaderList) {
     glLinkProgram(program);
     GLint status;
     glGetProgramiv (program, GL_LINK_STATUS, &status);
+
     if (status == GL_FALSE)
     {
         GLint infoLogLength;
@@ -67,19 +76,18 @@ GLuint GlHelper::CreateProgram(const std::vector<GLuint> &shaderList) {
 
         GLchar *strInfoLog = new GLchar[infoLogLength + 1];
         glGetProgramInfoLog(program, infoLogLength, NULL, strInfoLog);
-        fprintf(stderr, "Linker failure: %s\n", strInfoLog);
+        std::cerr << "Linkleme sorunu: \n" << strInfoLog << std::endl;
         delete[] strInfoLog;
     }
 
     offsetLocation = glGetUniformLocation(program, "offset");
-
     frustumScaleUnif = glGetUniformLocation(program, "frustumScale");
     zNearUnif = glGetUniformLocation(program, "zNear");
     zFarUnif = glGetUniformLocation(program, "zFar");
 
-
     for(size_t iLoop = 0; iLoop < shaderList.size(); iLoop++)
         glDetachShader(program, shaderList[iLoop]);
+
     return program;
 }
 
@@ -99,7 +107,7 @@ void GlHelper::InitializeVertexBuffer() {
 
     glBindBuffer(GL_ARRAY_BUFFER, positionBufferObject);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
-    //glEnableClientState(GL_VERTEX_ARRAY) ;
+    glEnableClientState(GL_VERTEX_ARRAY) ;
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     //coloring
@@ -107,124 +115,29 @@ void GlHelper::InitializeVertexBuffer() {
     glBindBuffer(GL_ARRAY_BUFFER, colorBufferObject) ;
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertexColors), vertexColors,GL_STATIC_DRAW);
     glColorPointer(4, GL_FLOAT, 0, 0) ;
-    //glEnableClientState(GL_COLOR_ARRAY) ;
+    glEnableClientState(GL_COLOR_ARRAY) ;
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
-    
+
 }
 
 
 GlHelper::GlHelper() {
-/*
+
     float vertexPositions[] = {
-//Front face
-                1.0f, 1.0f, 1.0f, 1.0f,
-                1.0f, -1.0f, 1.0f, 1.0f,
-                -1.0f, 1.0f, 1.0f, 1.0f,
-  
-                -1.0f, 1.0f, 1.0f, 1.0f,
-                -1.0f, -1.0f, 1.0f, 1.0f,
-                1.0f, -1.0f, 1.0f, 1.0f,
-//back face    
-                1.0f, 1.0f, -1.0f, 1.0f,
-                1.0f, -1.0f, -1.0f, 1.0f,
-                -1.0f, 1.0f, -1.0f, 1.0f,
-  
-                -1.0f, 1.0f, -1.0f, 1.0f,
-                -1.0f, -1.0f, -1.0f, 1.0f,
-                1.0f, -1.0f, -1.0f, 1.0f,
-//top face
-		1.0f, 1.0f, 1.0f, 1.0f,
-                1.0f, 1.0f, -1.0f, 1.0f,
-                -1.0f, 1.0f, -1.0f, 1.0f,
-
-                -1.0f, 1.0f, -1.0f, 1.0f,
-                -1.0f, 1.0f, 1.0f, 1.0f,
-                1.0f, 1.0f, 1.0f, 1.0f,
-//bottom face
-		1.0f, -1.0f, 1.0f, 1.0f,
-                1.0f, -1.0f, -1.0f, 1.0f,
-                -1.0f, -1.0f, -1.0f, 1.0f,
-
-                -1.0f, -1.0f, -1.0f, 1.0f,
-                -1.0f, -1.0f, 1.0f, 1.0f,
-                1.0f, -1.0f, 1.0f, 1.0f,
-//left face
-                -1.0f, 1.0f, 1.0f, 1.0f,
-                -1.0f, 1.0f, -1.0f, 1.0f,
-                -1.0f, -1.0f, -1.0f, 1.0f,
-
-                -1.0f, -1.0f, -1.0f, 1.0f,
-                -1.0f, -1.0f, 1.0f, 1.0f,
-                -1.0f, 1.0f, -1.0f, 1.0f,
-//right face
-                1.0f, 1.0f, 1.0f, 1.0f,
-                1.0f, 1.0f, -1.0f, 1.0f,
-                1.0f, -1.0f, -1.0f, 1.0f,
-
-                1.0f, -1.0f, -1.0f, 1.0f,
-                1.0f, -1.0f, 1.0f, 1.0f,
-                1.0f, 1.0f, -1.0f, 1.0f
-    };
-    
-    float vertexColors[] = {
-		1.0f, 0.0f, 0.0f, 1.0f,
-		1.0f, 0.0f, 0.0f, 1.0f,
-		1.0f, 0.0f, 0.0f, 1.0f,
-		1.0f, 0.0f, 0.0f, 1.0f,
-		1.0f, 0.0f, 0.0f, 1.0f,
-		1.0f, 0.0f, 0.0f, 1.0f,
-
-		1.0f, 0.0f, 0.0f, 1.0f,
-		1.0f, 0.0f, 0.0f, 1.0f,
-		1.0f, 0.0f, 0.0f, 1.0f,
-		1.0f, 0.0f, 0.0f, 1.0f,
-		1.0f, 0.0f, 0.0f, 1.0f,
-		1.0f, 0.0f, 0.0f, 1.0f,
-
-		0.0f, 1.0f, 0.0f, 1.0f,
-		0.0f, 1.0f, 0.0f, 1.0f,
-		0.0f, 1.0f, 0.0f, 1.0f,
-		0.0f, 1.0f, 0.0f, 1.0f,
-		0.0f, 1.0f, 0.0f, 1.0f,
-		0.0f, 1.0f, 0.0f, 1.0f,
-		
-		0.0f, 1.0f, 0.0f, 1.0f,
-		0.0f, 1.0f, 0.0f, 1.0f,
-		0.0f, 1.0f, 0.0f, 1.0f,
-		0.0f, 1.0f, 0.0f, 1.0f,
-		0.0f, 1.0f, 0.0f, 1.0f,
-		0.0f, 1.0f, 0.0f, 1.0f,
-
-		0.0f, 0.0f, 1.0f, 1.0f,
-		0.0f, 0.0f, 1.0f, 1.0f,
-		0.0f, 0.0f, 1.0f, 1.0f,
-		0.0f, 0.0f, 1.0f, 1.0f,
-		0.0f, 0.0f, 1.0f, 1.0f,
-		0.0f, 0.0f, 1.0f, 1.0f,
-
-		0.0f, 0.0f, 1.0f, 1.0f,
-		0.0f, 0.0f, 1.0f, 1.0f,
-		0.0f, 0.0f, 1.0f, 1.0f,
-		0.0f, 0.0f, 1.0f, 1.0f,
-		0.0f, 0.0f, 1.0f, 1.0f,
-		0.0f, 0.0f, 1.0f, 1.0f
-    };
-*/
-float vertexPositions[] = {
-         0.25f,  0.25f, -1.75f, 1.0f,
-         0.25f, -0.25f, -1.75f, 1.0f,
+        0.25f,  0.25f, -1.75f, 1.0f,
+        0.25f, -0.25f, -1.75f, 1.0f,
         -0.25f,  0.25f, -1.75f, 1.0f,
 
-         0.25f, -0.25f, -1.75f, 1.0f,
+        0.25f, -0.25f, -1.75f, 1.0f,
         -0.25f, -0.25f, -1.75f, 1.0f,
         -0.25f,  0.25f, -1.75f, 1.0f,
 
-         0.25f,  0.25f, -2.25f, 1.0f,
+        0.25f,  0.25f, -2.25f, 1.0f,
         -0.25f,  0.25f, -2.25f, 1.0f,
-         0.25f, -0.25f, -2.25f, 1.0f,
+        0.25f, -0.25f, -2.25f, 1.0f,
 
-         0.25f, -0.25f, -2.25f, 1.0f,
+        0.25f, -0.25f, -2.25f, 1.0f,
         -0.25f,  0.25f, -2.25f, 1.0f,
         -0.25f, -0.25f, -2.25f, 1.0f,
 
@@ -236,30 +149,30 @@ float vertexPositions[] = {
         -0.25f, -0.25f, -2.25f, 1.0f,
         -0.25f,  0.25f, -2.25f, 1.0f,
 
-         0.25f,  0.25f,  -1.75f, 1.0f,
-         0.25f, -0.25f, -2.25f, 1.0f,
-         0.25f, -0.25f,  -1.75f, 1.0f,
+        0.25f,  0.25f,  -1.75f, 1.0f,
+        0.25f, -0.25f, -2.25f, 1.0f,
+        0.25f, -0.25f,  -1.75f, 1.0f,
 
-         0.25f,  0.25f,  -1.75f, 1.0f,
-         0.25f,  0.25f, -2.25f, 1.0f,
-         0.25f, -0.25f, -2.25f, 1.0f,
+        0.25f,  0.25f,  -1.75f, 1.0f,
+        0.25f,  0.25f, -2.25f, 1.0f,
+        0.25f, -0.25f, -2.25f, 1.0f,
 
-         0.25f,  0.25f, -2.25f, 1.0f,
-         0.25f,  0.25f,  -1.75f, 1.0f,
+        0.25f,  0.25f, -2.25f, 1.0f,
+        0.25f,  0.25f,  -1.75f, 1.0f,
         -0.25f,  0.25f,  -1.75f, 1.0f,
 
-         0.25f,  0.25f, -2.25f, 1.0f,
+        0.25f,  0.25f, -2.25f, 1.0f,
         -0.25f,  0.25f,  -1.75f, 1.0f,
         -0.25f,  0.25f, -2.25f, 1.0f,
 
-         0.25f, -0.25f, -2.25f, 1.0f,
+        0.25f, -0.25f, -2.25f, 1.0f,
         -0.25f, -0.25f,  -1.75f, 1.0f,
-         0.25f, -0.25f,  -1.75f, 1.0f,
+        0.25f, -0.25f,  -1.75f, 1.0f,
 
-         0.25f, -0.25f, -2.25f, 1.0f,
+        0.25f, -0.25f, -2.25f, 1.0f,
         -0.25f, -0.25f, -2.25f, 1.0f,
         -0.25f, -0.25f,  -1.75f, 1.0f
-};
+    };
     float vertexColors[] = {
         0.0f, 0.0f, 1.0f, 1.0f,
         0.0f, 0.0f, 1.0f, 1.0f,
@@ -308,7 +221,8 @@ float vertexPositions[] = {
         0.0f, 1.0f, 1.0f, 1.0f,
         0.0f, 1.0f, 1.0f, 1.0f,
         0.0f, 1.0f, 1.0f, 1.0f
-};
+    };
+
 
     xOffset = 0.0f;
     yOffset = 0.0f;
@@ -321,14 +235,10 @@ float vertexPositions[] = {
 
     glGenVertexArrays(1, &vao);
     glBindVertexArray(vao);
-
-        glDisable(GL_CULL_FACE);
-        //glCullFace(GL_BACK);
-        //glFrontFace(GL_CW);
+    glDisable(GL_CULL_FACE);
 }
-
 void GlHelper::render() {
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
     glUseProgram(theProgram);
@@ -337,7 +247,7 @@ void GlHelper::render() {
     glUniform1f(frustumScaleUnif, 1.0f);
     glUniform1f(zNearUnif, 1.0f);
     glUniform1f(zFarUnif, 3.0f);
-  
+
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, positionBufferObject);
     glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
@@ -351,23 +261,26 @@ void GlHelper::render() {
     glUseProgram(0);
 }
 
-void GlHelper::move(GlHelper::directions direction){
-	float movementAmount = 0.1;
+void GlHelper::move(GlHelper::directions direction) {
+    float movementAmount = 0.1;
 
-	switch (direction) {
-	case UP:
-		yOffset += movementAmount;
-		break;
-	case DOWN:
-		yOffset -= movementAmount;
-		break;
-	case LEFT:
-		xOffset -= movementAmount;
-		break;
-	case RIGHT:
-		xOffset += movementAmount;
-		break;
-	}
+    switch (direction) {
+    case UP:
+        yOffset += movementAmount;
+        break;
+
+    case DOWN:
+        yOffset -= movementAmount;
+        break;
+
+    case LEFT:
+        xOffset -= movementAmount;
+        break;
+
+    case RIGHT:
+        xOffset += movementAmount;
+        break;
+    }
 
 }
 
--- 5/src/main.cpp
+++ 5/src/main.cpp
@@ -1,13 +1,9 @@
-//Copyright (C) 2010-2012 by Jason L. McKesson
-//This file is licensed under the MIT License.
-
 #include "GlfwHelper.h"
-
 
 int main(int argc, char** argv)
 {
     GlfwHelper glfwHelper;
     glfwHelper.loop();
-    
+
     return 0;
 }
