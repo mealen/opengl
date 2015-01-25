a06d002547cf895e5e0c06019978e17e9483bca8
Engin Manap <enginmanap@gmail.com>
2015-01-25 11:40:58 +0200

    add vao usage

---
+++ 9/src/GlfwHelper.h
@@ -0,0 +1,27 @@
+#ifndef GLUT_HELPER_H
+#define GLUT_HELPER_H
+
+//order is important
+
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+
+#include "GlHelper.h"
+#include <stdio.h>
+
+class GlfwHelper {
+
+private:
+    GLFWwindow* window;
+    GlHelper* openglHelper;
+
+public:
+    GlfwHelper();
+    void loop();
+    GlHelper *getGlHelper();
+};
+
+
+
+#endif
