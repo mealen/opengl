06f58f96616d45d3130c142d0738f038a377c62d
Engin Manap <enginmanap@gmail.com>
2015-12-15 20:47:31 +0200

    add ornek project

---
+++ ornek/src/GlHelper.h
@@ -0,0 +1,40 @@
+#ifndef GL_HELPER_H
+#define GL_HELPER_H
+
+#include <GL/glew.h>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <streambuf>
+#include <iostream>
+
+#ifdef LOAD_X11
+#define APIENTRY
+#endif
+
+class GlHelper {
+
+private:
+    GLuint positionBufferObject;
+    GLuint colorBufferObject;
+    GLuint vao;
+    GLuint theProgram;
+    GLuint vertexbuffer;
+    float vertexPositions[12];
+    float vertexColors[12];
+
+public:
+    GlHelper();
+    GLuint CreateShader(GLenum, const std::string &);
+    GLuint CreateProgram(const std::vector<GLuint> &);
+    void InitializeProgram();
+    void reshape (int, int);
+
+    void InitializeVertexBuffer();
+
+
+    void render();
+};
+
+#endif
